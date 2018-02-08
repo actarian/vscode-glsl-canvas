5559d1d87e00333756f6fe332aa9d2247bdf4e95
Luca Zampetti <lzampetti@gmail.com>
2018-02-08 08:02:58 +0100

    vscode-glsl-canvas

--- src/shader.js
+++ src/shader.js
@@ -30,6 +30,21 @@
             for (var p in window.textures) {
                 glsl.setUniform('u_texture_' + p, window.textures[p]);
             }
+
+            var stats = new Stats();
+            stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+            document.body.appendChild(stats.dom);
+
+            function animate() {
+                stats.update();
+                requestAnimationFrame(animate);
+                // stats.begin();
+                // monitored code goes here
+                // stats.end();
+                // requestAnimationFrame(animate);
+                // stats.begin();
+            }
+            requestAnimationFrame(animate);
         }
 
         function onResize() {
